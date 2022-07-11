455da3ad7ce9ffb5b39f0781d26a083c6532300b
skymins04 <skymin0417@gmail.com>
2022-07-12 04:41:52 +0900

    cafelist 요청시 불필요한 오버헤드 수정

--- backend/functions/cafelist.ts
+++ backend/functions/cafelist.ts
@@ -15,6 +15,68 @@ const failResponse = {
 const MAX_BOARD_NUM = 5;
 
 /**
+ * 네이버카페 글목록 파싱 함수
+ * @param {string} cafeId
+ * @param {string} title
+ * @param {string} menuId
+ * @param {number} count
+ * @param {boolean} skipNotice
+ * @returns
+ */
+const parseNaverCafe = async (
+  cafeId: string,
+  title: string,
+  menuId: string,
+  count: number,
+  skipNotice: boolean
+) => {
+  const url = `https://cafe.naver.com/ArticleList.nhn?search.clubid=${cafeId}&search.menuid=${menuId}&userDisplay=${count}&search.boardtype=L&search.cafeId=${cafeId}&search.page=1`;
+  const $content = $.load(
+    await axios
+      .default({ url, method: "GET", responseType: "arraybuffer" })
+      .then((res) => iconv.decode(res.data, "EUC-KR"))
+  );
+
+  const $noticeArticleList = $content(
+    "div.article-board div.inner_list a.article"
+  );
+  const $normalArticleList = $content(
+    "div.article-board:not(#upperArticleList) div.inner_list a.article"
+  );
+
+  const articles: { title: string; url: string; isNotice: boolean }[] = [];
+  if (!skipNotice) {
+    $noticeArticleList.each((idx, ele) => {
+      articles.push({
+        title: $content(ele)
+          .text()
+          .replace(/(\n|\t)/g, "")
+          .trim()
+          .replace(/ +/g, " "),
+        url: `https://cafe.naver.com${$content(ele).attr("href")}`,
+        isNotice: true,
+      });
+    });
+  }
+  $normalArticleList.each((idx, ele) => {
+    articles.push({
+      title: $content(ele)
+        .text()
+        .replace(/(\n|\t)/g, "")
+        .trim()
+        .replace(/ +/g, " "),
+      url: `https://cafe.naver.com${$content(ele).attr("href")}`,
+      isNotice: false,
+    });
+  });
+  return {
+    title,
+    articles,
+    type: "naver-cafe",
+  };
+};
+
+/**
  * 네이버카페 글목록을 파싱하여 Response로 전달하는 핸들러
  * @param event
  * @param context
@@ -29,72 +91,48 @@ const handler: Handler = async (event, context) => {
   if (event.body === null) return failResponse;
 
   // URL 유효성 검사
-  const { boards } = JSON.parse(event.body);
+  const { cafeId, boards } = JSON.parse(event.body);
+  const checkedBoards = [];
 
   if (!(boards instanceof Array) || boards.length === 0) return failResponse;
   for (const { idx, board } of boards.map((board, idx) => ({ idx, board }))) {
     if (idx === MAX_BOARD_NUM) break;
     else if (
       typeof board.title !== "string" ||
-      typeof board.url !== "string" ||
+      typeof board.menuId !== "string" ||
+      typeof board.count !== "number" ||
+      typeof board.type !== "string" ||
       typeof board.skipNotice !== "boolean"
     )
       return failResponse;
-    else if (
-      !board.url.match(
-        /^https:\/\/cafe\.naver\.com\/ArticleList\.nhn\?search\.clubid=[0-9]+&search\.menuid=[0-9]+&userDisplay=[0-9]+&search\.boardtype=L&search.cafeId=[0-9]+&search.page=1$/
-      )
-    )
-      return failResponse;
+    checkedBoards.push(board);
   }
 
   // 각 URL 별 네이버카페 글목록 데이터 파싱
 
   const data = [];
-  for (const { title, url, skipNotice } of boards) {
-    const $content = $.load(
-      await axios
-        .default({ url, method: "GET", responseType: "arraybuffer" })
-        .then((res) => iconv.decode(res.data, "EUC-KR"))
-    );
-
-    const $noticeArticleList = $content(
-      "div.article-board div.inner_list a.article"
-    );
-    const $normalArticleList = $content(
-      "div.article-board:not(#upperArticleList) div.inner_list a.article"
-    );
-
-    const articles: { title: string; url: string; isNotice: boolean }[] = [];
-    if (!skipNotice) {
-      $noticeArticleList.each((idx, ele) => {
-        articles.push({
-          title: $content(ele)
-            .text()
-            .replace(/(\n|\t)/g, "")
-            .trim()
-            .replace(/ +/g, " "),
-          url: `https://cafe.naver.com${$content(ele).attr("href")}`,
-          isNotice: true,
-        });
-      });
+  for (const { title, menuId, count, type, skipNotice } of checkedBoards) {
+    let c = count;
+    switch (count) {
+      case 5:
+      case 10:
+      case 15:
+      case 20:
+      case 30:
+      case 40:
+      case 50:
+        break;
+      default:
+        c = 15;
+        break;
     }
-    $normalArticleList.each((idx, ele) => {
-      articles.push({
-        title: $content(ele)
-          .text()
-          .replace(/(\n|\t)/g, "")
-          .trim()
-          .replace(/ +/g, " "),
-        url: `https://cafe.naver.com${$content(ele).attr("href")}`,
-        isNotice: false,
-      });
-    });
-    data.push({
-      title,
-      articles,
-      type: "naver-cafe",
-    });
+    switch (type) {
+      case "naver-cafe":
+        data.push(
+          await parseNaverCafe(cafeId.naverCafe, title, menuId, c, skipNotice)
+        );
+        break;
+    }
   }
 
   return {
