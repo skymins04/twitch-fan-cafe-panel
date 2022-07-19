993ba953dab2a04ba0141727601e5bee45f0b7cb
skymins04 <skymin0417@gmail.com>
2022-07-19 19:31:36 +0900

    메타데이터를 게시글 항목 하단에 표시 추가 & 팬카페 소유자의 작성글일 경우 owner 클래스를 적용

--- front/panel.js
+++ front/panel.js
@@ -169,13 +169,21 @@ function getCafeList(callback) {
               : "";
             const tabContentItem = $("<li></li>");
             const tabContentItemTitle = $(
-              `<span class="title">${noticeTag}<span class="idx">${
+              `<div class="title">${noticeTag}<span class="idx">${
                 j + 1
               }</span><span class="dot">.</span><span class="text">${
                 article.articleTitle
-              }</span></span>`
+              }</span></div>`
             );
+            const tabContentItemMeta = $(`
+              <div class="meta">
+                <span class="date"><svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"/></svg><span class="text">${article.date}</span></span>
+                <span class="comment"><svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 32C114.6 32 .0272 125.1 .0272 240c0 49.63 21.35 94.98 56.97 130.7c-12.5 50.37-54.27 95.27-54.77 95.77c-2.25 2.25-2.875 5.734-1.5 8.734C1.979 478.2 4.75 480 8 480c66.25 0 115.1-31.76 140.6-51.39C181.2 440.9 217.6 448 256 448c141.4 0 255.1-93.13 255.1-208S397.4 32 256 32z"/></svg><span class="text">${article.cmt}</span></span>
+                <span class="author"><svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M421.7 220.3L188.5 453.4L154.6 419.5L158.1 416H112C103.2 416 96 408.8 96 400V353.9L92.51 357.4C87.78 362.2 84.31 368 82.42 374.4L59.44 452.6L137.6 429.6C143.1 427.7 149.8 424.2 154.6 419.5L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3zM492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75z"/></svg><span class="text">${article.author}</span</span>
+              </div>
+            `);
             tabContentItem.append(tabContentItemTitle);
+            tabContentItem.append(tabContentItemMeta);
             tabContentItem.attr("data-clipboard-text", article.url);
             tabContentItem.attr("data-article-no", j + 1);
             tabContentItem.addClass("item");
@@ -185,6 +193,7 @@ function getCafeList(callback) {
             tabContentItem.addClass(`ran4_${randomNum(0, 3)}`);
             tabContentItem.addClass(`ran5_${randomNum(0, 5)}`);
             if (article.isNotice) tabContentItem.addClass("item-notice");
+            if (article.isOwner) tabContentItem.addClass("owner");
             tabContent.append(tabContentItem);
           });
 
